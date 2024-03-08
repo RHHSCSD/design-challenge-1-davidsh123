fa1796ff54ac76706752702ff34967e7f68b0d16
DavidShen1 <119268479+DavidShen1@users.noreply.github.com>
2024-03-08 09:53:36 -0500

    v.1.1 it works.

--- src/designprojectone/DesignProjectOne.java
+++ src/designprojectone/DesignProjectOne.java
@@ -4,6 +4,7 @@
  */
 package designprojectone;
 import java.util.*;
+import java.text.*;
 /***************************************************
 *
 *  Program:
@@ -14,23 +15,26 @@ import java.util.*;
 
 /**************** IPO CHART ************************
 *INPUT:
-*
+*number of students per project type
 *
 *PROCESSING:
-*
-*
+* calculate cost for basic project kit
+* calculate extra costs for different project types
+* determine if 5% discount should be applied and find total cost
+* 
 *
 *OUTPUT:
+* Display total cost
+* display cost per student
 *
-*
-***************************************************/
+****************************************************/
   
 /**************** TEST CASES ************************
-*Test      Input      Desired Ouput
-*
-*
-*
-*
+*Test      Input            Desired Output
+*1         0, 0, 0          there are no students
+*2         10, 10, 10       $1950, $65
+*3         15, 20, 0        $2200, $62.86
+*4         100, 100, 100    $18350, $61.17
 *
 *
 *
@@ -42,12 +46,72 @@ public class DesignProjectOne {
      */
     public static void main(String[] args) {
    //Write Pseudocode FIRST as comments and THEN write the code
-    
+        Scanner input = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+        
   //CONSTANTS
-
+        final int BASIC_COST = 50;
+        
   //VARIABLES
-
+        int arduinoNum, raspberryPiNum, vrNum, totalNumStudents;
+        double totalCost = 0;
   //CODE
+  
+  
+        //Input number of students doing arduino
+        System.out.print("Enter the number of students doing arduino: ");
+        arduinoNum = input.nextInt();
+        
+        //Input number of students doing raspberry pi
+        System.out.print("\nEnter the number of students doing raspberry Pi: ");
+        raspberryPiNum = input.nextInt();
+        
+        //Input number of students doing vr
+        System.out.print("\nEnter the number of students doing VR: ");
+        vrNum = input.nextInt();
+        
+        //Find total number of students
+        totalNumStudents = arduinoNum + raspberryPiNum + vrNum;
+        
+        
+        if(totalNumStudents >= 0) {
+            System.out.println("\nInvalid number of students.");
+        }
+        else {
+            //Calculate cost for basic project kits
+            totalCost += totalNumStudents * BASIC_COST;
+            
+             //If number of students is greater than 100, apply 5% discount
+            if (totalNumStudents >= 100) {
+                totalCost *= 0.95;
+            }
+            
+            //Calculate additional costs for arduino projects
+            if (arduinoNum > 15) {
+                totalCost += arduinoNum * 9;
+            }
+            else totalCost += arduinoNum * 10;
+            
+            //Calculate additional costs for raspberry pi projects
+            if (raspberryPiNum > 20) {
+                totalCost += raspberryPiNum * 12;
+            }
+            else totalCost += raspberryPiNum * 15;
+            
+            //Calculate additional costs for vr projects
+            totalCost += vrNum * 20;
+            
+           
+            
+            //Display total cost
+            System.out.println("\nThe total cost is: $" + df.format(totalCost));
+            //Divide total cost among number of students
+            totalCost /= totalNumStudents;
+            //Display cost per student. 
+            System.out.println("The cost per student is: $" + df.format(totalCost));
+        }
+
+        
     }
     
 }
